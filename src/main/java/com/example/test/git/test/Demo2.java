00e287f3e10aed4f16bf9d8da5ecc99d3fd5edb1
HLF <2455306703@qq.com>
2023-12-20 19:14:02 +0800

    version 0.0.1 工程初始化版本

---
+++ src/main/java/com/example/test/git/test/Demo2.java
@@ -0,0 +1,4 @@
+package com.example.test.git.test;
+
+public class Demo2 {
+}
---
+++ src/main/java/com/example/test/git/test/GitTestDemo.java
@@ -0,0 +1,9 @@
+package com.example.test.git.test;
+
+public class GitTestDemo {
+
+    public static void main(String[] args) {
+        System.out.println("git first!");
+    }
+
+}
