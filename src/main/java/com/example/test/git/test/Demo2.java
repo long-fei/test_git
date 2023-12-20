4247a2827b76f8a324adedaab8a5aaccb1363fb0
HLF <2455306703@qq.com>
2023-12-20 20:55:44 +0800

    version 0.0.1 工程初始化版本-2023-12-20 修改版本三-添加了Gitee

--- src/main/java/com/example/test/git/test/Demo2.java
+++ src/main/java/com/example/test/git/test/Demo2.java
@@ -4,7 +4,7 @@ public class Demo2 {
 
     public static void main(String[] args) {
         System.out.println("第二个案例这个是");
-        System.out.println("添加了Gitee！");
+        System.out.println("添加了Gitee！！");
     }
 
 }
